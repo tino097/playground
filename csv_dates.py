835482c4743b36c62a0bcdb9ae9b9dcbe47e6204
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2017-12-26 20:13:29 +0100

    finsih

--- csv_dates.py
+++ csv_dates.py
@@ -2,8 +2,8 @@
 ''' @author Konstantin Sivakov'''
 
 import requests
-import json
 import csv
+from datetime import datetime
 
 
 api_key = 'fe52241347d4855f7bc8cd70cd00cb6d'
@@ -13,18 +13,27 @@ def api_call(api_key, freq='D'):
     ''' Make API call '''
     url = 'http://api.eia.gov/series/?api_key={0}&series_id=NG.RNGWHHD.D'.format(api_key)
     result = requests.get(url)
-    return json.loads(result.content)
+    return result.json()
 
-def csv_writer(res, name=None, bom=False):
+
+def parse_results(results):
+    print type(result)
+    data = result['series'][0]
+    return data.get('data')
+
+
+def csv_writer(data, name=None, bom=False):
     with open('results.csv', 'wb+') as csv_file:
-        headers = "date, price".split(",")
-        writer = csv.DictWriter(csv_file, headers, delimiter=',')
+        fieldnames = ['date', 'price']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
-        series = dict(res['series'])
-        data = series['data']
-        print data
-        # writer.writerows(res['data'])
-        
+        for t in data:
+            date, price = datetime.strptime(t[0], '%Y%m%d'), t[1]
+            row = {'date': date.date(), 'price': price}      
+            writer.writerow(row)
+
+
 if __name__ == '__main__':
-    result = api_call(api_key,"M")
-    csv_writer(result)
+    result = api_call(api_key, "M")
+    data = parse_results(result)
+    csv_writer(data)
