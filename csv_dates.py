cc7d6dab725ede3ac90cc19b951be0e0424efb0a
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2017-12-27 00:13:27 +0100

    small updates

--- csv_dates.py
+++ csv_dates.py
@@ -1,29 +1,45 @@
 # encoding: utf-8
-''' @author Konstantin Sivakov'''
+''' simple  script to consume API data and convert to CSV file
+    usage: python csv_dates.py [--api-key --freq]
+'''
 
 import requests
 import csv
+import argparse
 from datetime import datetime
 
+parse = argparse.ArgumentParser()
+parse.add_argument('--api-key', help='Enter your api key!')
+parse.add_argument('--freq', help='Frequency of prices (daily or monthly)')
 
-api_key = 'fe52241347d4855f7bc8cd70cd00cb6d'
+args = parse.parse_args()
+freq = args.freq or 'D'
+api_key = args.api_key or 'fe52241347d4855f7bc8cd70cd00cb6d'
+
+result_file = 'results'
+
+if freq == 'D':
+    result_file = result_file + '_daily.csv'
+else:
+    result_file = result_file + '_monthly.csv'
 
 
 def api_call(api_key, freq='D'):
     ''' Make API call '''
-    url = 'http://api.eia.gov/series/?api_key={0}&series_id=NG.RNGWHHD.D'.format(api_key)
+    url = 'http://api.eia.gov/series/?api_key={0}&series_id=NG.RNGWHHD.{1}'.format(api_key, freq)
     result = requests.get(url)
+    result.raise_for_status()
     return result.json()
 
 
 def parse_results(results):
-    print type(result)
-    data = result['series'][0]
+    data = results['series'][0]
     return data.get('data')
 
 
-def csv_writer(data, name=None, bom=False):
-    with open('results.csv', 'wb+') as csv_file:
+def csv_writer(data,  filename='results_daily.csv'):
+    ''' csv writer method '''
+    with open(filename, 'wb+') as csv_file:
         fieldnames = ['date', 'price']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
@@ -34,6 +50,6 @@ def csv_writer(data, name=None, bom=False):
 
 
 if __name__ == '__main__':
-    result = api_call(api_key, "M")
+    result = api_call(api_key, freq)
     data = parse_results(result)
-    csv_writer(data)
+    csv_writer(data, filename=result_file)
--- thinkpy/mypoligon.py
+++
@@ -1,7 +0,0 @@
-from swampy.TurtleWorld import *
-
-world = TurtleWorld()
-bob = Turtle()
-print bob
-
-wait_for_user()
